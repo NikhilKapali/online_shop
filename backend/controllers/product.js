38510094d945f1d5944f318499e81233e82143f4
NikhilKapali <nikhilkapali12@gmail.com>
2021-03-14 21:05:57 +0545

    Product create/validation/authorization/ delete

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -5,8 +5,24 @@ const _ = require("lodash");
 const fs = require("fs");
 
 const { errorHandler } = require("../helpers/dbErrHandler");
-const { RSA_NO_PADDING } = require("constants");
 
+
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: 'Product Not Found'
+            });
+        }
+        req.product = product;
+        next();
+    });
+}
+
+exports.read = (req, res, next) => {
+    req.product.photo = undefined;
+    return res.json(req.product)
+}
 
 exports.create = (req, res) => {
     const form = new formidable.IncomingForm()
@@ -17,9 +33,28 @@ exports.create = (req, res) => {
                 error: 'Image could not be uploaded'
             });
         }
+        //check for all fields
+        const { name, description, price, category, quantity, shipping } = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required!!'
+            });
+        }
+
+
         let product = new Product(fields);
 
+        //1kb = 1000
+        //1mb = 1000000
+
         if (files.photo) {
+            // console.log("FILES_PHOTO:", files.photo);
+            if (files.photo.size > 5000000) {
+                return res.status(400).json({
+                    error: 'Image size should be less than 5mb'
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type
         }
@@ -35,3 +70,18 @@ exports.create = (req, res) => {
 
     })
 }
+
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            "message": "Product deleted"
+        })
+    })
+}
+
