946bcb073b2847247e164771598770fbfe406ee1
NikhilKapali <nikhilkapali12@gmail.com>
2021-03-15 22:45:22 +0545

    Product Listing/sort/limit

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -126,3 +126,30 @@ exports.update = (req, res) => {
 
     })
 }
+
+//getting products based on sales and arrival
+//returning/getting products bySell = /products?sortBy=sold&order=desc&limit=4
+//returning/getting products byArrival = /products?sortBy=createdAt&order=desc&limit=4
+//if no params are sent, then get/return all products
+
+
+exports.list = (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc'
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    Product.find()
+        .select("-photo")
+        .populate('category')
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Products not Found'
+                });
+            }
+            res.send(products);
+        })
+
+}
